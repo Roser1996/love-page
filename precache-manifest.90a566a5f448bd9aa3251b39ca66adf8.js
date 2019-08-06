02a4eb021d190ac401e72004fda5bf28cac8d432
RuiGan <rgan1@uci.edu>
2019-08-06 10:47:59 -0700

    Updates

---
+++ precache-manifest.90a566a5f448bd9aa3251b39ca66adf8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f292b4dfba9a733275b20e5c628335d6",
+    "url": "/love-page/index.html"
+  },
+  {
+    "revision": "a906ca2385ccd838cdd4",
+    "url": "/love-page/static/css/main.395de7ca.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/love-page/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a906ca2385ccd838cdd4",
+    "url": "/love-page/static/js/main.0c43c9ae.chunk.js"
+  },
+  {
+    "revision": "c9ff3f43cedf1832d32d",
+    "url": "/love-page/static/js/runtime~main.52cf72f9.js"
+  }
+]);
