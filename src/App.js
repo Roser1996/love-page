69a04645c63d186a21610f19fc9269c025374a96
RuiGan <rgan1@uci.edu>
2019-08-06 10:28:02 -0700

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      clicked: false
+    }
+  }
+
+  handleClick = () => {
+    this.setState({ clicked: true });
+  }
+
+  render() {
+    const { clicked } = this.state;
+
+    if (clicked) {
+      return (
+        <div>
+          <div id="heart"></div>
+          <h1>Happy Chinese Valentines day</h1>
+        </div>
+      );
+    }
+    else {
+      return (
+        <div className="myBtn">
+          <button type="button" onClick={this.handleClick}>
+            Suprise! Click Me!
+          </button>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
